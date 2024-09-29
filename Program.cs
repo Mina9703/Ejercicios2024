19a410659dc570e225968897675c2a6d68ae1ba9
Michelle <ibarracortezmichellenahomy@gmail.com>
2024-09-29 16:36:18 -0600

    Agrega programa de Ejercicio1 para calcular la diferencia entre dos números

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("Por favor ingrese el primer número:");
+        double primerNumero = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Por favor ingrese el segundo número:");
+        double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = CalcularDiferencia(primerNumero, segundoNumero);
+        Console.WriteLine($"El resultado es: {resultado}");
+    }
+
+    static double CalcularDiferencia(double num1, double num2)
+    {
+        double diferencia = Math.Abs(num1 - num2);
+        if (num1 > num2)
+        {
+            return diferencia * 2;
+        }
+        return diferencia;
+    }
+}
+
+
