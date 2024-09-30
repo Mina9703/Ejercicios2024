cc6d9e70d6f0924acd226b895839e9d0d774a0f4
Michelle <ibarracortezmichellenahomy@gmail.com>
2024-09-29 18:23:47 -0600

    Número impar

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números impares del 1 al 99:\n");
+
+        for (int i = 1; i < 100; i += 2)
+        {
+            Console.WriteLine($"El número {i} es impar");
+        }
+    }
+}
